2e27a9fbac423be208cc202000a2d6bce250a769
Scott Rowland <scrowlan@gmail.com>
2012-12-16 14:09:29 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bankroll::Application.config.secret_token = '6485d3d38ace3102edf1c5cf6e451357c5b4ec7391a1552f7954db8bc9b346bc45d00b7485de786bd4cda1c806d98d2e50c9dfb22d145ba02ac464e81829ee73'
