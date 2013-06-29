23126f44b676f8e211a687da1cd75eab664c0611
Scott Rowland <scrowlan@gmail.com>
2013-06-29 08:50:37 -0700

    make games dependent on rolls for destroy

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -1,7 +1,7 @@
 class Roll < ActiveRecord::Base
   attr_accessible :amount, :site_name
   belongs_to :user
-  has_many :games
+  has_many :games, dependent: :destroy
   
   validates :user_id, presence: true
   
@@ -9,12 +9,12 @@ class Roll < ActiveRecord::Base
     self.total_difference_by_game = (Game.sum(:difference, :group => :roll_id)).fetch(id)
   end
   
-  def updated_bankroll(id)
-    @updated_bankroll = Roll.find_by_id(id).amount + @total_difference_by_game = (Game.sum(:difference, :group => :roll_id)).fetch(id)
-  end
-  
   def tot_hours_by_game(id)
     self.total_hours_by_game = (Game.sum(:hours_played, :group => :roll_id)).fetch(id)
+  end
+  
+  def updated_bankroll(id)
+    @updated_bankroll = Roll.find_by_id(id).amount + @total_difference_by_game = (Game.sum(:difference, :group => :roll_id)).fetch(id)
   end
 
   def hourly_rate
