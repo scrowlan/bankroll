1033e5e5a46735984997323023b68ab376b5519e
Scott Rowland <scrowlan@gmail.com>
2013-11-28 09:56:04 -0800

    updating login/signup redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       flash[:success] = "Welcome to the Poker Bankroll Tracker!"
-      redirect_to @user
+      redirect_to root_url
     else
       render 'new'
     end
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
-      redirect_to @user
+      redirect_to root_url
     else
       render 'edit'
     end
